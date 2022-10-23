ec323242f816e7db9e99bbb330e338e9be5aa7bf
mbuguaj254 <mbuguaj58@gmail.com>
2022-10-23 21:31:37 +0300

    An update of the app feature. I have included django models of the artistes, songs and lyrics.

---
+++ songcrud/musicapp/models.py
@@ -0,0 +1,19 @@
+from turtle import title
+from django.db import models
+
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
